be4199d0a655f7c6462433b6a693310b694cd282
CarbonicKevin <46237721+CarbonicKevin@users.noreply.github.com>
2020-06-02 00:34:54 -0400

    stuff idk

--- python/1.2.CalcArm.py
+++ python/1.2.CalcArm.py
@@ -51,14 +51,18 @@ class arm:
 
         self.alpha = np.pi-self.theta
 
-        self.p2Angle = 2*np.pi-(self.alpha + self.gamma2)
-
-        if abs(self.p2Angle) > np.pi/2: sign = 1
+        if abs(2*np.pi-(self.alpha + self.gamma2)) > np.pi/2: sign = 1
         else: sign = -1
 
-        self.p0Angle = self.theta+sign*self.gamma0
+        self.p0Angle = (self.theta+sign*self.gamma0)
+        self.p1Angle = (np.pi - self.gamma1)
+        self.p2Angle = self.gamma2-(2*np.pi-(self.alpha + sign*self.gamma2)-(2*np.pi-2*self.gamma2)/2)
 
         self.p1 = self.p0 + np.array([self.l1*np.cos(self.p0Angle), self.l1*np.sin(self.p0Angle)])
+
+        self.p0Angle = self.p0Angle * (180/np.pi) * -1
+        self.p1Angle = self.p1Angle * (180/np.pi) * (-1*sign)
+        self.p2Angle = self.p2Angle * (180/np.pi) * -1
 
 
     def plt(self):
@@ -97,17 +101,21 @@ class arm:
 
         ax.plot([self.p2[0], self.delta[0]+self.p2[0]], [self.p2[1], self.p2[1]], 'r', label="End Effector")
 
+        textstr = "P0 Angle: {:.4}\nP1 Angle: {:.4}\nP2 Angle: {:.4}".format(self.p0Angle, self.p1Angle, self.p2Angle)
+        props = dict(boxstyle='round', facecolor='red', alpha=0.5)
+        ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=14, verticalalignment='top', bbox=props)
+
         ax.axis('square')
         ax.legend()
         plt.savefig("C:/Users/kevin/Documents/GitHub/Praxis-3-2020-Winter-Individual/python/images/{},{}.png".format(self.p2[0]+self.delta[0], self.p2[1]))
 
-a = arm([60,60], 10)
+a = arm([60,60], 13.3447, p0=[0,0])
 
-a.invKin(np.array([100,  50]))
+a.invKin(np.array([120,  50]))
 a.plt()
-a.invKin(np.array([100,   0]))
+a.invKin(np.array([120,   0]))
 a.plt()
-a.invKin(np.array([100, -50]))
+a.invKin(np.array([120, -50]))
 a.plt()
 
 a.invKin(np.array([ 75,  50]))
